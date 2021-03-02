6c3a745041969a417b02419712438993943dcc3c
evanjjones <evanjones@fastmail.net>
2021-03-02 17:20:47 -0500

    fix margins on blog

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import moment from 'moment';
+import styled from '@emotion/styled';
+
+const PostContainer = styled.div`
+  margin: 2rem;
+`;
 
 export default function BlogPost({ post }) {
   const date = moment(post.frontmatter.date).format('MMMM Do YYYY, h:mm a');
   return (
-    <div>
+    <PostContainer>
       <h1>{post.frontmatter.title}</h1>
       <h3>{post.frontmatter.description}</h3>
       <h3>{date}</h3>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
+    </PostContainer>
   );
 }
