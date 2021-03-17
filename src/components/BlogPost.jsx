f9a20509d8192e6951fe519cee23c821b5a5a8e6
evanjjones <evanjones@fastmail.net>
2021-03-17 13:39:58 -0400

    change font size and lin height

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -11,13 +11,18 @@ const PostContainer = styled.div`
   }
 `;
 
+const HTMLContainer = styled.div`
+  font-size: 1.25rem;
+  line-height: 26px;
+`;
+
 export default function BlogPost({ post }) {
   return (
     <PostContainer>
       <h1>{post.frontmatter.title}</h1>
       <h3>{post.frontmatter.description}</h3>
       <h3>{post.frontmatter.date}</h3>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <HTMLContainer dangerouslySetInnerHTML={{ __html: post.html }} />
     </PostContainer>
   );
 }
